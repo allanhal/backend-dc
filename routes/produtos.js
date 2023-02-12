b8fb569da0c73c200d38174ad231ac4234454714
Wesley Azevedo <azevedosou@gmail.com>
2023-02-12 16:12:05 -0300

    comentei onde cada dupla irá trabalhar

--- routes/produtos.js
+++ routes/produtos.js
@@ -3,17 +3,38 @@ var router = express.Router();
 var fs = require('fs');
 const { path } = require('../app');
 
+
+// Listar produtos Lucas e Wesley
 router.get('/', function(req, res, next) {
     res.send('Lista de produtos')
 });
+
+
+
+// Listar produtos por ID Guilherme e Hélio
+router.get('/', function(req, res, next) {
+    res.send('Lista de produtos')
+});
+
+
+
+// Criar produtos Deivid e Igor
 router.post('/', function(req, res, next) {
     res.send('Criar produto')
 });
+
+
+
+// Atualizar produtos André e Rian
 function update(req, res, next) {
     res.send('Alterar/Update produto ' + req.params.id)
 }
 router.put('/:id', update);
 router.patch('/:id', update);
+
+
+
+// Apagar produtos Emmanuel
 router.delete('/:id', function(req, res, next) {
     fs.readFile('./data/produtos.json',"utf8", (err, data) => {
         const produtos = JSON.parse(data)
@@ -28,6 +49,7 @@ router.delete('/:id', function(req, res, next) {
     })
     // res.send('Remove produto ' + req.params.id)
 });
+
 
 
 module.exports = router;
