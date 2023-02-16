9c5ec2262c3b0a13aa90ac06c042f1e3a83170ce
Davi Roque <sowdavi@gmail.com>
2023-02-16 14:16:27 -0300

    post product

--- routes/produtos.js
+++ routes/produtos.js
@@ -6,73 +6,115 @@ const { path } = require('../app');
 
 
 //ROTA BUSCANDO TODOS OS PRODUTOS DO ARQUIVO LIDO COM O MODULO FS
-router.get('/', function(req, res, next) {
-    fs.readFile('./data/produtos.json',"utf-8",(err, data)=> {
+router.get('/', function (req, res, next) {
+    fs.readFile('./data/produtos.json', "utf-8", (err, data) => {
 
         const produtos = JSON.parse(data)
         res.send(produtos)
-    
+
     })
-    
+
 });
 //Rota para busca por palavra-chave
-router.get('/search',function(req,res,next){
-    fs.readFile('./data/produtos.json',"utf-8",(err, data)=> {
-        if(err){
+router.get('/search', function (req, res, next) {
+    fs.readFile('./data/produtos.json', "utf-8", (err, data) => {
+        if (err) {
             res.send("Ocorreu um erro na sua busca")
         }
-        try{
+        try {
             const produtos = JSON.parse(data)
-            const {palavra_chave,info} = req.query
-           
-            
-            let produtoBuscado = produtos.filter((produto)=>produto[`${palavra_chave}`].toString().toLowerCase() === info)
-            if(produtoBuscado){
+            const { palavra_chave, info } = req.query
+
+
+            let produtoBuscado = produtos.filter((produto) => produto[`${palavra_chave}`].toString().toLowerCase() === info)
+            if (produtoBuscado) {
                 res.send(produtoBuscado)
-            }else{
+            } else {
                 res.send("Nenhum produto encontrado para esssas especificacoes")
             }
 
-        }catch(erro){
+        } catch (erro) {
             res.send("Ocorreu um erro:", erro)
         }
-    
+
     })
 })
 //--------------------------------------
 //Rota para buscar um produto individualmente 
-router.get('/:id', function(req, res, next) {
-    fs.readFile('./data/produtos.json',"utf-8",(err, data)=> {
-        const {id}= req.params
-        
-         try{const produtos = JSON.parse(data)
-          
-        const produtoSelecionado = produtos.find((produto)=>produto.id===id)
-        if(produtoSelecionado){
-            res.send(produtoSelecionado)
-        }else{
-            res.send("Nenhum produto encontrado para essa especificação")
+router.get('/:id', function (req, res, next) {
+    fs.readFile('./data/produtos.json', "utf-8", (err, data) => {
+        const { id } = req.params
+
+        try {
+            const produtos = JSON.parse(data)
+
+            const produtoSelecionado = produtos.find((produto) => produto.id === id)
+            if (produtoSelecionado) {
+                res.send(produtoSelecionado)
+            } else {
+                res.send("Nenhum produto encontrado para essa especificação")
+            }
         }
-       }
-       catch{
-        res.send('Ocorreu um erro:'+err)
-       }
-         
-    
+        catch {
+            res.send('Ocorreu um erro:' + err)
+        }
+
+
     })
-    
+
 });
 
-router.post('/', function(req, res, next) {
-    res.send('Criar produto')
+//Rota para adicionar um produto
+router.post('/', function (req, res, next) {
+    fs.readFile('./data/produtos.json', 'utf8', (err, data) => {
+        try {
+            const produtos = JSON.parse(data)
+            let novoID = null
+            let maiorID = 0
+            produtos.forEach((element) => {
+                if (eval(element.id) > maiorID) {
+                    maiorID = eval(element.id)
+                }
+                novoID = maiorID + 1
+                console.log(maiorID)
+            });
+            const produtoCriado = {
+                "categoria": req.body.categoria,
+                "titulo": req.body.titulo,
+                "descricao": req.body.descricao,
+                "valor": req.body.valor,
+                "desconto": req.body.desconto,
+                "img": req.body.img,
+                "id": String(novoID)
+            }
+            if (produtoCriado !== {}) {
+                produtos.push(produtoCriado)
+                if (produtoCriado !== {}) {
+                    fs.writeFileSync('./data/produtos.json', JSON.stringify(produtos))
+                    res.send('produto adicionado com o id ' + novoID + ' criado com sucesso')
+                }
+                else {
+                    res.send('erro ao criar produto, os itens contém o mesmo Identificado Único (' + novoID + ')')
+                }
+            }
+            else {
+                res.send('nao foi possível criar o produto, erro (' + err + ')')
+            }
+        }
+        catch {
+            res.send('ocorreu um erro -' + err)
+        }
+
+    })
 });
+
 function update(req, res, next) {
     res.send('Alterar/Update produto ' + req.params.id)
 }
 router.put('/:id', update);
 router.patch('/:id', update);
-router.delete('/:id', function(req, res, next) {
-    fs.readFile('./data/produtos.json',"utf8", (err, data) => {
+router.delete('/:id', function (req, res, next) {
+    fs.readFile('./data/produtos.json', "utf8", (err, data) => {
         const produtos = JSON.parse(data)
         const id = req.params.id
 
