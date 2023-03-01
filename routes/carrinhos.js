b536f167ef38700a79fdc59665e64bc76fcc89de
Emerson Souza <souzaemerson003@gmail.com>
2023-02-28 22:42:29 -0300

    adicionando a funcao de edicao dos carrinhos disponíveis

--- routes/carrinhos.js
+++ routes/carrinhos.js
@@ -1,73 +1,102 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var fs = require('fs');
-
+var fs = require("fs");
 
 //mostrando todos os carrinhos
-router.get('/', function (req, res, next) {
-    const carrinho = JSON.parse(fs.readFileSync('./data/carrinho.json', 'utf-8'));
-    res.json(carrinho);
+router.get("/", function (req, res, next) {
+  const carrinho = JSON.parse(fs.readFileSync("./data/carrinho.json", "utf-8"));
+  res.json(carrinho);
 });
 
 // Rota para calcular o valor total do carrinho
-router.get('/:id/valortotal', (req, res, next) => {
-    const carrinhoId = req.params.id; // Pega o ID do carrinho da rota
-    console.log(`id do carrim ` + carrinhoId)
+router.get("/:id/valortotal", (req, res, next) => {
+  const carrinhoId = req.params.id; // Pega o ID do carrinho da rota
+  console.log(`id do carrim ` + carrinhoId);
 
-    // Lê o arquivo de produtos
-    const produtos = JSON.parse(fs.readFileSync('./data/produtos.json', 'utf-8'));
+  // Lê o arquivo de produtos
+  const produtos = JSON.parse(fs.readFileSync("./data/produtos.json", "utf-8"));
 
-    // Lê o arquivo de carrinhos
-    const carrinhos = JSON.parse(fs.readFileSync('./data/carrinho.json', 'utf-8'));
-    const carrinho = carrinhos.find((c) => c.id === carrinhoId);
+  // Lê o arquivo de carrinhos
+  const carrinhos = JSON.parse(
+    fs.readFileSync("./data/carrinho.json", "utf-8")
+  );
+  const carrinho = carrinhos.find((c) => c.id === carrinhoId);
 
-    if (!carrinho) {
-        return res.status(404).send('Carrinho não encontrado');
+  if (!carrinho) {
+    return res.status(404).send("Carrinho não encontrado");
+  }
+
+  let valorFinal = 0;
+
+  // Percorre todos os produtos do carrinho
+  carrinho.produtos.forEach((produtoNoCarrinho) => {
+    const produto = produtos.find((p) => p.id === produtoNoCarrinho.produto_id);
+    console.log("produtoooooooo valor " + produto.valor);
+    console.log("produtooooooo descontoooooo " + produto.desconto);
+
+    // Calcula o valor total do produto com desconto
+    const valorComDesconto = (produto.valor * (100 - produto.desconto)) / 100;
+    console.log("valoorcomdesconto@@", valorComDesconto);
+    valorFinal = valorComDesconto * produtoNoCarrinho.quantidade + valorFinal;
+    console.log("carin quantidade", produtoNoCarrinho.quantidade);
+    console.log("OOOOOOOOO", valorFinal);
+
+    if (!produto) {
+      return res
+        .status(400)
+        .send(
+          `Produto não encontrado para o ID ${produtoNoCarrinho.produto_id}`
+        );
+    }
+  });
+
+  //const descontoTotal = carrinho.desconto / 100 * valorFinal;
+  console.log("carinho desconto", carrinho.desconto);
+  //console.log("desconto total", descontoTotal)
+  totalDesconto = valorFinal * (carrinho.desconto / 100);
+  valorFinal -= totalDesconto;
+  res.send(`Valor total do carrinho ${carrinhoId}: R$${valorFinal.toFixed(2)}`);
+});
+
+router.get("/:id", function (req, res, next) {
+  res.send("Exibe apenas um carrinho");
+});
+
+router.post("/", function (req, res, next) {
+  res.send("Criar novo carrinho");
+});
+
+// Rota para editar os itens do carrinho
+router.patch("/:id", function (req, res, next) {
+    // Le o arquivo JSON com os carrinhos disponíveis
+  fs.readFile("./data/carrinho.json", "utf8", (err, data) => {
+    const carrinhos = JSON.parse(data);
+    const id = req.params.id;
+
+    const carrinhoEditado = carrinhos.find((carrinho) => carrinho.id === id)
+
+    // if (req.body === JSON.parse( )) {
+    //     res.status(304).send("nenhuma mudança foi realizada")
+    //     return
+    // }
+
+    if (!carrinhoEditado) {
+        res.status(404).send("Carrinho não encontrado.")
+        return
     }
 
-    let valorFinal = 0;
 
-    // Percorre todos os produtos do carrinho
-    carrinho.produtos.forEach((produtoNoCarrinho) => {
-        const produto = produtos.find((p) => p.id === produtoNoCarrinho.produto_id);
-        console.log("produtoooooooo valor " + produto.valor)
-        console.log("produtooooooo descontoooooo " + produto.desconto)
+    Object.assign(carrinhoEditado, req.body)
 
-        // Calcula o valor total do produto com desconto
-        const valorComDesconto = produto.valor * (100 - produto.desconto) / 100;
-        console.log("valoorcomdesconto@@", valorComDesconto)
-        valorFinal = valorComDesconto * produtoNoCarrinho.quantidade + valorFinal
-        console.log("carin quantidade", produtoNoCarrinho.quantidade)
-        console.log("OOOOOOOOO", valorFinal)
+    fs.writeFileSync('./data/carrinho.json', JSON.stringify(carrinhos))
 
-        if (!produto) {
-            return res.status(400).send(`Produto não encontrado para o ID ${produtoNoCarrinho.produto_id}`);
-        }
-    });
+    res.send("carrinho com o id "+ id + " editado com sucesso! as novas informações são:" + req.body)
 
-    //const descontoTotal = carrinho.desconto / 100 * valorFinal;
-    console.log("carinho desconto", carrinho.desconto)
-    //console.log("desconto total", descontoTotal)
-    totalDesconto = valorFinal * (carrinho.desconto / 100)
-    valorFinal -= totalDesconto
-    res.send(`Valor total do carrinho ${carrinhoId}: R$${valorFinal.toFixed(2)}`);
+  });
 });
 
-router.get('/:id', function (req, res, next) {
-    res.send("Exibe apenas um carrinho")
+router.delete("/:id", function (req, res, next) {
+  res.send("deleta um carrinho");
 });
-
-router.post('/', function (req, res, next) {
-    res.send('Criar novo carrinho')
-});
-
-router.patch('/:id', function (req, res, next) {
-    res.send('edita um carrinho')
-});
-
-router.delete('/:id', function (req, res, next) {
-    res.send('deleta um carrinho')
-});
-
 
 module.exports = router;
